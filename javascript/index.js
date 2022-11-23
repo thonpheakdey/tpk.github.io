5bcd63f32c39d5a51daff65969898d00fa0d6153
thonpheakdey <109382716+thonpheakdey@users.noreply.github.com>
2022-11-24 01:32:45 +0700

    1.0.0.2

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,28 @@
-function onclicklink4(){
-    location.href = "html/solar.html";
+function onclicklink4(m) {
+    let koj = document.createElement("div");
+    koj.style.width = "705px";
+    koj.style.height = "100%";
+    koj.style.backgroundColor = "red";
+    koj.id = "ko";
+    befor.appendChild(koj);
+    let ko = document.getElementById("ko1");
+    ko.remove();
+    // location.href = "html/solar.html";
 }
-function onclicklink5(){
-    location.href = "html/homesytem.html";
+function onclicklink5(a) {
+   {
+    let koj = document.createElement("div");
+    koj.style.width = "705px";
+    koj.id = "ko1";
+    koj.style.height = "100%";
+    koj.style.float = "right";
+    koj.style.backgroundColor = "black";
+    befor.appendChild(koj);
+    let ko = document.getElementById("ko");
+    ko.remove();
+   } 
+    
+
 }
 function onclicklink1() {
     location.href = "https://web.facebook.com/pheakdey.com3766";
