c801000da6db977d8abff69e5d1bfe93bb48d394
thonpheakdey <109382716+thonpheakdey@users.noreply.github.com>
2024-06-19 00:58:56 +0700

    19/06/2024

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,7 @@
 
-function home() {
-    let home = document.querySelector("iframe");
-    home.src = "/html/home.html";
-}
-function project() {
-    let project = document.querySelector("iframe");
-    project.src = "/html/project.html";
-}
-function about() {
-    let project = document.querySelector("iframe");
-    project.src = "/html/about.html";
-}
-function onclicklink1() {
-    location.href = "https://web.facebook.com/pheakdey.com3766";
-}
-function onclicklink2() {
-    location.href = "https://m.me/pheakdey.com3766";
-}
-// function home() {
-//     let home = document.getElementById("home");
-//     home.remove();
+// function onclicklink1() {
+//     location.href = "https://web.facebook.com/pheakdey.com3766";
+// }
+// function onclicklink2() {
+//     location.href = "https://m.me/pheakdey.com3766";
 // }
