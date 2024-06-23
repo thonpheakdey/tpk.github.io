190a5672f7fabcec7401d5a57e531b4c569472e8
thonpheakdey <109382716+thonpheakdey@users.noreply.github.com>
2024-06-23 23:37:50 +0700

    23/06/2024

--- javascript/product.js
+++ javascript/product.js
@@ -2,8 +2,39 @@ function onclicklink() {
     location.href = "../index.html";
 }
 
+const div1 = document.getElementById('product');
+const div2 = document.getElementById('data_send');
+
+function makeDiv1Big() {
+    div1.style.flex = '5';
+    div2.style.flex = '1';
+}
+
+function makeDiv2Big() {
+    div1.style.flex = '2';
+    div2.style.flex = '2';
+}
+
+div1.addEventListener('mousemove', makeDiv1Big);
+div2.addEventListener('mousemove', makeDiv2Big);
+
+document.addEventListener('mousemove', (event) => {
+    const rect1 = div1.getBoundingClientRect();
+    const rect2 = div2.getBoundingClientRect();
+    
+    if (!(rect1.left <= event.clientX && event.clientX <= rect1.right && rect1.top <= event.clientY && event.clientY <= rect1.bottom) &&
+        !(rect2.left <= event.clientX && event.clientX <= rect2.right && rect2.top <= event.clientY && event.clientY <= rect2.bottom)) {
+        div1.style.flex = '1';
+        div2.style.flex = '1';
+    }
+});
+
 
 async function sendMessage() {
+    let idchack = ["kraken", "sasquatch", "mothman"];
+    for (let g = 0; g < 3; g++) {
+        console.log("jof");
+    };
     const name = document.getElementById('name').value;
     const quantity = document.getElementById('quantity').value;
     const Checkbox = document.getElementById('checkbox').checked;
