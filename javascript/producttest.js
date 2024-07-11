cd6b95d5aef2404225440eb544ace234e6889136
thonpheakdey <109382716+thonpheakdey@users.noreply.github.com>
2024-07-11 23:54:46 +0700

    11/07/2024

---
+++ javascript/producttest.js
@@ -0,0 +1,53 @@
+// //input
+// {
+//     let rel = document.createElement('input');
+//     let inputw = document.createElement('div');
+//     let inputh = document.createElement('input');
+//     let output = document.createElement('output');
+
+//     // let br = document.createElement('br');
+//     //inputw
+//     // inputw.type = "number";
+//     // inputw.placeholder = "ចំនួនអានុភាព (W)";
+//     inputw.id = "inputw1";
+//     inputw.className = "input";
+//     inputw.style.backgroundColor = "red" ;
+//     inputw.style.width = "100px";
+//     inputw.style.height = "100px";
+    
+//     //inputh
+//     inputh.type = "number";
+//     inputh.placeholder = "ចំនួនម៉ោង (h)";
+//     inputh.id = "inputh1";
+//     inputh.className = "input";
+//     //
+//     output.id = "output1";
+//     output.className = "input";
+//     output.innerText = "Wh";
+//     output.style.backgroundColorz = "red";
+//     //rel
+//     rel.type = "Number";
+//     rel.placeholder = "ចំនួនទឹកប្រាក់ក្នុងKWH =1500 រៀល";
+//     rel.id = "rel";
+//     rel.className = "input";
+//     list_product.appendChild(rel);
+//     list_product.appendChild(inputw);
+//     list_product.appendChild(inputh);
+//     list_product.appendChild(output);
+
+// }
+for(let i =0;i<30;i++){
+    let star = document.createElement('div');
+    star.id = "star";
+    star.className = "star";
+    list_product.appendChild(star);
+
+    let star1 = document.createElement('div');
+    star1.className = "star1";
+    star.appendChild(star1);
+}
+// window.onclick = function (){
+
+// }
+
+
